9023d67f3c61dc721c2056741357f9df637365a6
masche14 <162661048+masche14@users.noreply.github.com>
2024-05-09 11:10:06 +0900

    Add files via upload

---
+++ study05059.sql
@@ -0,0 +1,3 @@
+insert into emp06
+values(7566,'JONES','MANAGER',50)
+/
---
+++ study05509.sql
@@ -0,0 +1,7 @@
+create table emp02(
+empno number(4) not null
+, ename varchar2(10) not null,
+job varchar2(9)
+,deptno number(2)
+)
+/
